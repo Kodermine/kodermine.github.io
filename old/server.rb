e84bcf8d00ae61a345c4a0ccfa19dcbcead2d7e1
Florida Elago <florida.elago@gmail.com>
2014-05-18 22:17:42 -0700

    remove old

--- old/server.rb
+++
@@ -1,9 +0,0 @@
-require 'webrick'
- 
-include WEBrick
-port = 2000
- 
-puts "Starting server: http://#{Socket.gethostname}:#{port}"
-server = HTTPServer.new(:Port=>2000,:DocumentRoot=>Dir::pwd )
-trap("INT"){ server.shutdown }
-server.start
